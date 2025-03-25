a8071d04abdaa1cd7ea925e1fd7bb76706bbcd73
Oleksandr Boiko <oleksandr.boiko@mythical.games>
2025-03-25 22:06:53 +0000

    trying to use versioneer

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
 import setuptools
+import versioneer
 
-setuptools.setup(name="my")
+setuptools.setup(name="my",
+                 version=versioneer.get_version(),
+                 cmdclass=versioneer.get_cmdclass())
